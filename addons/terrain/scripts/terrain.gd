a03ad0755b39ef958ea6de907d216b6f81f503a1
Jonathan Moallem <jdmoallem@icloud.com>
2024-01-08 23:38:52 +1000

    Implemented terrain generation with height handles and face selection for UV modification

--- addons/terrain/scripts/terrain.gd
+++ addons/terrain/scripts/terrain.gd
@@ -1,40 +1,60 @@
 @tool
 class_name Terrain extends StaticBody3D
 
-const tris = [
-	Vector3(-1, 0, 1),
-	Vector3(-1, 0, -1),
-	Vector3(1, 0, 1),
-	
-	Vector3(1, 0, 1),
-	Vector3(-1, 0, -1),
-	Vector3(1, 0, -1),
-]
-
-@export var tri_uvs = [
-	Vector2(0.1, 0.1),
-	Vector2(0.8, 0.8),
-]
+@export var size: float = 1.0
+@export_range(1, 1000) var rows: int = 1
+@export_range(1, 1000) var cols: int = 1
 
 @export var material: BaseMaterial3D
 
-@export var vert_mods = [
-	0.5,
-	0,
-	0,
-	0.5,
-]
+@export var uv_ids: Dictionary = {}
 
-func get_tri(idx: int) -> Array[Vector3]:
-	var tri_idx = idx*3
+@export var default_uv: Vector2
+
+@export var height_mappings: Dictionary = {}
+
+@export var uv_mappings: Dictionary = {}
+
+func get_tri_by_idx(idx: int) -> Array:
+	var row: int = idx/(rows*2)
+	var col: int = (idx/2)%rows
+	var verts := get_verts_at_row_col(row, col)
+	return [verts[2], verts[1], verts[0]] if idx % 2 == 0 else [verts[3], verts[1], verts[2]]
+
+func get_verts_at_row_col(row: int, col: int) -> Array[Vector3]:
 	return [
-		tris[tri_idx],
-		tris[tri_idx+1],
-		tris[tri_idx+2]
+		Vector3(col*size, get_height_at(row, col, 0), row*size),
+		Vector3(col*size, get_height_at(row, col, 1), (row+1)*size),
+		Vector3((col+1)*size, get_height_at(row, col, 2), row*size),
+		Vector3((col+1)*size, get_height_at(row, col, 3), (row+1)*size),
 	]
 
+func get_height_at(row: int, col: int, idx: int) -> float:
+	var curr_row: int = row*rows
+	var nxt_row: int = (row+1)*rows
+	var rc_sum: int = col + row
+	match(idx):
+		0:
+			return height_mappings.get(curr_row + rc_sum, 0.0)
+		1:
+			return height_mappings.get(nxt_row + rc_sum + 1, 0.0)
+		2:
+			return height_mappings.get(curr_row + rc_sum + 1, 0.0)
+		3:
+			return height_mappings.get(nxt_row + rc_sum + 2, 0.0)
+	return 0
+
+func get_unique_verts() -> Array[Vector3]:
+	var verts: Array[Vector3]
+	var idx: int = 0
+	for row in range(rows+1):
+		for col in range(cols+1):
+			verts.append(Vector3(col*size, height_mappings.get(idx, 0.0), row*size))
+			idx += 1
+	return verts
+
 func get_tri_count() -> int:
-	return tri_uvs.size()
+	return rows*cols*2
 
 func reset():
 	for child in get_children():
@@ -43,20 +63,38 @@ func reset():
 func generate_mesh():
 	var mesh_instance: MeshInstance3D = find_or_create_mesh_instance()
 	
-	var st = SurfaceTool.new()
+	var st := SurfaceTool.new()
 	st.begin(Mesh.PRIMITIVE_TRIANGLES)
 	
-	for i in range(get_tri_count()):
-		st.set_uv(tri_uvs[i])
-		var tri = get_tri(i)
-		st.add_vertex(tri[0] + Vector3(0, vert_mods[i], 0))
-		st.add_vertex(tri[1] + Vector3(0, vert_mods[i+1], 0))
-		st.add_vertex(tri[2] + Vector3(0, vert_mods[i+2], 0))
+	for row in range(rows):
+		for col in range(cols):
+			var verts := get_verts_at_row_col(row, col)
+			var uvs := get_uvs_for_row_col(row, col)
+			st.set_uv(uvs[0])
+			st.add_vertex(verts[2])
+			st.add_vertex(verts[1])
+			st.add_vertex(verts[0])
+			
+			st.set_uv(uvs[1])
+			st.add_vertex(verts[3])
+			st.add_vertex(verts[1])
+			st.add_vertex(verts[2])
 	
 	st.generate_normals()
 	var mesh = st.commit()
 	mesh_instance.mesh = mesh
 	mesh_instance.material_override = material
+	mesh_instance.position = get_centre_offset()
+
+func get_uvs_for_row_col(row: int, col: int) -> Array[Vector2]:
+	var plane_idx: int = row*rows + row*2 + col*2
+	return [
+		uv_ids.get(uv_mappings.get(plane_idx, default_uv), default_uv),
+		uv_ids.get(uv_mappings.get(plane_idx + 1, default_uv), default_uv)
+	]
+
+func get_centre_offset() -> Vector3:
+	return -(Vector3(rows, 0, cols) * size / 2)
 
 func generate_collision():
 	var mesh_instance: MeshInstance3D = find_mesh_instance()
@@ -65,6 +103,7 @@ func generate_collision():
 	
 	var collision_shape: CollisionShape3D = find_or_create_collision_shape()
 	collision_shape.shape = mesh_instance.mesh.create_trimesh_shape()
+	collision_shape.position = get_centre_offset()
 
 func find_mesh_instance() -> MeshInstance3D:
 	var mesh_instance: MeshInstance3D
--- addons/terrain/scripts/terrain_gizmo_plugin.gd
+++ addons/terrain/scripts/terrain_gizmo_plugin.gd
@@ -15,24 +15,27 @@ func _redraw(gizmo: EditorNode3DGizmo):
 	var terrain: Terrain = gizmo.get_node_3d() as Terrain
 	
 	var handles: PackedVector3Array = PackedVector3Array()
-	handles.push_back(Vector3(1, terrain.vert_mods[1], 1))
-	handles.push_back(Vector3(-1, terrain.vert_mods[3], 1))
-	handles.push_back(Vector3(-1, terrain.vert_mods[2], -1))
-	handles.push_back(Vector3(1, terrain.vert_mods[0], -1))
-	gizmo.add_handles(handles, get_material("handles", gizmo), [0, 1, 2, 3])
+	var verts: Array[Vector3] = terrain.get_unique_verts()
+	var offset: Vector3 = terrain.get_centre_offset()
+	for i in range(verts.size()):
+		handles.push_back(verts[i] + offset)
+	gizmo.add_handles(handles, get_material("handles", gizmo), range(verts.size()))
 	
 	var selected_subgizmo = gizmo.get_subgizmo_selection()
 	if not selected_subgizmo.is_empty():
 		var mat = get_material("face_selection", gizmo)
-		var mesh = SphereMesh.new()
-		var tri = terrain.get_tri(selected_subgizmo[0])
+		var tri = terrain.get_tri_by_idx(selected_subgizmo[0])
 		var x = (tri[0].x + tri[1].x + tri[2].x) / 3
 		var y = (tri[0].y + tri[1].y + tri[2].y) / 3
 		var z = (tri[0].z + tri[1].z + tri[2].z) / 3
 		var xform: Transform3D
-		xform.origin = Vector3(x, y, z)
+		xform.origin = Vector3(x, y, z) + offset
+		var mesh = SphereMesh.new()
+		mesh.radius = 0.25
+		mesh.height = 0.5
 		gizmo.add_mesh(mesh, mat, xform)
 	
+	# TODO offset these somehow
 	var mesh_instance: MeshInstance3D = terrain.find_mesh_instance()
 	gizmo.add_collision_triangles(mesh_instance.mesh.generate_triangle_mesh())
 
@@ -44,11 +47,11 @@ func _get_subgizmo_transform(gizmo: EditorNode3DGizmo, subgizmo_id: int):
 func _subgizmos_intersect_ray(gizmo: EditorNode3DGizmo, camera: Camera3D, screen_pos: Vector2):
 	var terrain: Terrain = gizmo.get_node_3d() as Terrain
 	for i in range(terrain.get_tri_count()):
-		var tri = terrain.get_tri(i)
+		var tri = terrain.get_tri_by_idx(i)
 		var origin: Vector3 = camera.project_ray_origin(screen_pos)
 		var direction: Vector3 = camera.project_ray_normal(screen_pos)
-		if Geometry3D.ray_intersects_triangle(origin, direction, tri[0], tri[1], tri[2]):
-			print("Selected: ", i)
+		var offset = terrain.get_centre_offset()
+		if Geometry3D.ray_intersects_triangle(origin, direction, tri[0] + offset, tri[1] + offset, tri[2] + offset):
 			return i
 	return -1
 
@@ -56,10 +59,14 @@ func _get_handle_name(gizmo: EditorNode3DGizmo, handle_id: int, secondary: bool)
 	return str(handle_id)
 
 func _get_handle_value(gizmo: EditorNode3DGizmo, handle_id: int, secondary: bool):
-	return "val_" + str(handle_id)
+	var terrain: Terrain = gizmo.get_node_3d() as Terrain
+	return terrain.height_mappings.get(handle_id, 0.0)
 
 func _commit_handle(gizmo: EditorNode3DGizmo, handle_id: int, secondary: bool, restore: Variant, cancel: bool):
-	pass
+	var terrain: Terrain = gizmo.get_node_3d() as Terrain
+	terrain.generate_mesh()
 
 func _set_handle(gizmo: EditorNode3DGizmo, handle_id: int, secondary: bool, camera: Camera3D, screen_pos: Vector2):
-	pass
+	var terrain: Terrain = gizmo.get_node_3d() as Terrain
+	var val: float = terrain.height_mappings.get(handle_id, 0.0)
+	terrain.height_mappings[handle_id] = val + 0.001

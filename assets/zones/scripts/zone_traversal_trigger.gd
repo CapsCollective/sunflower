dc1df9c4d5f6201e8bdb9e9455b5b3c7be30b58c
Jonathan Moallem <jdmoallem@icloud.com>
2025-03-24 19:55:59 +1000

    Updated zone traversal triggers to use zone id values for referencing

--- assets/zones/scripts/zone_traversal_trigger.gd
+++ assets/zones/scripts/zone_traversal_trigger.gd
@@ -1,6 +1,8 @@
 class_name ZoneTraversalTrigger extends Area3D
 
-@export_file("*.scn", "*.tscn") var traversal_zone: String
+const levels_dt = preload("res://assets/datatables/tables/levels_dt.tres")
+
+@export var traversal_zone_id: StringName
 @export var spawn_location: StringName
 
 func _ready():
@@ -9,9 +11,12 @@ func _ready():
 func on_body_entered(body: Node3D):
 	if body is not PlayerCharacter:
 		return
+	
 	body.input_enabled = false
 	body_entered.disconnect(on_body_entered)
 	for child in get_children():
 		if child is WorldCharacterAction:
 			child.run_action(body)
-	GameManager.game_world.load_level(traversal_zone, {"spawn_location": spawn_location})
+	
+	var row: LevelRow = levels_dt.get_row(traversal_zone_id)
+	GameManager.game_world.load_level(row.path, {"spawn_location": spawn_location})
